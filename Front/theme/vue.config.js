21deb221e7ba06f232bf07dc119224b4b65811ca
lion1735 <lion1735@naver.com>
2022-11-17 10:05:42 +0900

    deploy : 네이버 검색엔진 등록 테스트 #S07P31C203-162

--- Front/theme/vue.config.js
+++ Front/theme/vue.config.js
@@ -2,3 +2,30 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+const path = require('path');
+const PrerenderSpaPlugin = require('prerender-spa-plugin');
+
+const productionPlugins = [
+  new PrerenderSpaPlugin({
+    staticDir: path.join(__dirname, 'dist'),
+    routes: [
+        "/",
+        "/themelist",
+        "/search",
+        "/profile",
+    ],
+    renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
+      renderAfterElementExists: '#app'
+    }),
+  }),
+];
+
+module.exports = {
+  lintOnSave: false,
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(...productionPlugins);
+    }
+  },
+};
